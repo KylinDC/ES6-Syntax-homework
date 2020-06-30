fbd47adced01581a58d6ace04f3fe8d2df1ef3d5
KylinDC <inlovingdays@gmail.com>
2020-06-30 22:53:21 +0800

    feat: finish practice 1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const parseData = ({ data, column }) =>
+  data.map((rowData) => parseRowData({ rowData, column }));
+
+const parseRowData = ({ rowData, column }) =>
+  column.reduce(
+    (acc, cur, index) => ({ ...acc, [cur.name]: rowData[index] }),
+    {}
+  );
+
 export { parseData };
